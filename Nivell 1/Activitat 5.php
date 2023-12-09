baea4554c07d5df625e56525735e7cddbd42f183
Marta Villalba <marta.villalba.orta@gmail.com>
2023-12-09 12:09:38 +0100

    Arreglo en ejercicio 5

--- Nivell 1/Activitat 5.php
+++ Nivell 1/Activitat 5.php
@@ -8,9 +8,9 @@ function grado_estudio($nota)
 {
     if ($nota >= 60) {
         echo "Corresponde a la primera división";
-    } elseif ($nota >= 45 && $nota <= 59) {
+    } elseif ($nota >= 45) {
         echo "Corresponde a la segunda división";
-    } elseif ($nota >= 33 && $nota <= 44) {
+    } elseif ($nota >= 33) {
         echo "Corresponde a la tercera división";
     } else {
         echo "Tienes que volver a probar";
